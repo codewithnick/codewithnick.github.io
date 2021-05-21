fde8b0f2bc37f4d4c4c2fd77933c94f5c524dd0a
nikhil <nikhilsingh526452@gmail.com>
2021-05-21 15:32:47 +0530

    updated

--- project.js
+++ project.js
@@ -10,15 +10,24 @@ window.onload=()=>{
         webskilltab.classList.remove('hide');
         desktopskilltab.classList.add('hide');
         othersskillstab.classList.add('hide');
+        webskill.style.backgroundColor='rgba(118, 120, 237, 0.4)';
+        desktopskill.style.backgroundColor='transparent';
+        othersskill.style.backgroundColor='transparent';
     };
     desktopskill.onclick=()=>{
         desktopskilltab.classList.remove('hide');
         webskilltab.classList.add('hide');
         othersskillstab.classList.add('hide');
+        webskill.style.backgroundColor='transparent';
+        desktopskill.style.backgroundColor='rgba(118, 120, 237, 0.4)';
+        othersskill.style.backgroundColor='transparent';
     };
     othersskill.onclick=()=>{
         othersskillstab.classList.remove('hide');
         webskilltab.classList.add('hide');
         desktopskilltab.classList.add('hide');
+        othersskill.style.backgroundColor='rgba(118, 120, 237, 0.4)';
+        webskill.style.backgroundColor='transparent';
+        desktopskill.style.backgroundColor='transparent';
     };
 };
